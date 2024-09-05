2f55be2f40a586693956100bb7b530fcb2de4e1c
Tyler Cook <twc35917@uga.edu>
2024-09-05 10:24:19 -0400

    Created blinkC library and two files

---
+++ BlinkLibrary/blinkBcm2835.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "bcm2835.h"
+
+
+int main(){
+
+	printf("Hello World from Group 5 \n");
+
+}
---
+++ BlinkLibrary/blinkClass.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "E4235.h"
+
+int main(){
+
+	printf("Hello World from Group 5 \n");
+
+}
