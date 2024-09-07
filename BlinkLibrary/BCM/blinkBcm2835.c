71e6a1468041524223eba6a655050ab1aa053348
Tyler Cook <twc35917@uga.edu>
2024-09-07 18:29:03 -0400

    added -lbcm2835 flag to compile command

--- BlinkLibrary/BCM/blinkBcm2835.c
+++ BlinkLibrary/BCM/blinkBcm2835.c
@@ -2,7 +2,7 @@
 #include <bcm2835.h>
 #include <unistd.h>
 #include <gpiotopin.h>
-//gcc -o blinkBcm2835 blinkBcm2835.c -lbcm2835 for compile
+//gcc -o blinkBcm2835 blinkBcm2835.c -lbcm2835 -wall for compile
 
 int main(){
 	double freq = 0;
