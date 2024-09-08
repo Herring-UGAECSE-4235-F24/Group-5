b235c73e1bbd055f36529e255e6078833fa0f77c
Tyler-W-Cook <twc35917@uga.edu>
2024-09-07 20:26:22 -0400

    troubleshooting still

--- BlinkLibrary/BCM/blinkBcm2835.c
+++ BlinkLibrary/BCM/blinkBcm2835.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <bcm2835.h>
 #include <unistd.h>
-#include <gpiotopin.h>
+
 //gcc -o blinkBcm2835 blinkBcm2835.c -lbcm2835 -wall for compile
 
 int main(){
-	double freq = 0;
+	double freq;
 	printf("Enter frequency: ");
-	scanf("%d", &freq); //read int for freq
-
+	scanf("%lf", &freq); //read int for freq
+	printf("made it to loop");
 	while(1){
 	bcm2835_gpio_write(4, 1); //1 = high
-	sleep(1/freq);
+	usleep(1/frequency);
 	bcm2835_gpio_write(4, 0); //0 = low
 	}
 }
