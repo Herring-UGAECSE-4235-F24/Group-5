8bd65b66630151f25b0e8c52222b68686bebcc57
Tyler-W-Cook <twc35917@uga.edu>
2024-09-08 17:59:52 -0400

    Still having issues accessing BCM lib

--- BlinkLibrary/BCM/blinkBcm2835.c
+++ BlinkLibrary/BCM/blinkBcm2835.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <bcm2835.h>
+#include "bcm2835.h"
 #include <unistd.h>
 
+#define PIN RPI_GPIO_P1_03
 //gcc -o blinkBcm2835 blinkBcm2835.c -lbcm2835 -Wall for compile
+ // Set the pin to be an output
 
 int main(){
+	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
 	int freq;
     	printf("Enter Freq in Hz: ");
     	if (scanf("%d", &freq) != 1) {
@@ -14,10 +17,12 @@ int main(){
     	}
 
 	printf("your freq is %d", freq);
-	
+
 	while(1){
-	//bcm2835_gpio_write(4, 1); //1 = high
-	usleep(freq);
-	//bcm2835_gpio_write(4, 0); //0 = low
+	bcm2835_gpio_write(PIN , HIGH); //1 = high
+
+	bcm2835_delay(500);		//delay in mils
+	
+	bcm2835_gpio_write(PIN , LOW); //0 = low
 	}
 }
