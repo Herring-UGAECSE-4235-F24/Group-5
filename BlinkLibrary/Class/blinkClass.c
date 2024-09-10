7bf31861005030f3940ecc7fff3c6c644dbfb189
Tyler-W-Cook <twc35917@uga.edu>
2024-09-09 20:05:57 -0400

    Both Libraries are functional and blink the LED just need to colect data and characterize the SW now

--- BlinkLibrary/Class/blinkClass.c
+++ BlinkLibrary/Class/blinkClass.c
@@ -1,11 +1,17 @@
 #include "E4235.h"
 #include <stdio.h>
+//gcc blinkClass.c -o blinkClass -lE4235
+
 int main() {
-E4235_Select(11, 1); // set GPIO 12 to output
+E4235_Select(17, 1); // set GPIO 17 to output
 while (1) {
-	E4235_Write(11, 1);
-	E4235_Delaynano(500000);
-	E4235_Write(11, 0);
-	E4235_Delaynano(500000);
+	E4235_Write(17, 1);
+	printf("on");
+	E4235_Delaynano(500000000);
+	printf("reached");
+	E4235_Write(17, 0);
+	printf("off");
+	E4235_Delaynano(500000000);
+	printf("reached");
 }
 }
