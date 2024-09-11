85bdc0afaf63738644702c8b48dcecff92a5f24c
Tyler-W-Cook <twc35917@uga.edu>
2024-09-10 22:16:37 -0400

    Can't find class lib error

--- BlinkLibrary/Class/blinkClass.c
+++ BlinkLibrary/Class/blinkClass.c
@@ -6,12 +6,12 @@ int main() {
 E4235_Select(17, 1); // set GPIO 17 to output
 while (1) {
 	E4235_Write(17, 1);
-	printf("on");
-	E4235_Delaynano(500000000);
-	printf("reached");
+	
+	E4235_Delaynano(10000000);
+	
 	E4235_Write(17, 0);
-	printf("off");
-	E4235_Delaynano(500000000);
-	printf("reached");
+	
+	E4235_Delaynano(10000000);
+	
 }
 }
