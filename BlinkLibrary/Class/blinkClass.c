cf9254c184613a90d591d2773da6b855fe1b2eda
Tyler Cook <twc35917@uga.edu>
2024-09-11 21:54:58 -0400

    "pls plse"

--- BlinkLibrary/Class/blinkClass.c
+++ BlinkLibrary/Class/blinkClass.c
@@ -1,23 +1,21 @@
 #include "E4235.h"
 #include <stdio.h>
 //gcc blinkClass.c -o blinkClass -lE4235
-
+extern int E4235_Write ( int GPIO, int state );
+extern int E4235_Select ( int GPIO, int state );
+extern void E4235_DelayMicro ( int count )
 
 int main() {
 E4235_Select(17, 1); // set GPIO 17 to output
-int pins[1] = {17};
+
 while (1) {
-	E4235_multiwrite(pins,1, 1);
+	E4235_Write(17, 1);
 	
-	E4235_Delaynano(10000000);
+	E4235_DelayMicro(10000);
 	
-	E4235_multiwrite(pins, 1, 0);
+	E4235_Write(17, 0);
 	
-	E4235_Delaynano(10000000);
-
-	
-
-	
+	E4235_DelayMicro(10000);
 	
 }
 return 0;
