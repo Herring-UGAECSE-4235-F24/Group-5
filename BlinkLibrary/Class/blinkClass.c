707c3aeec75f2cb96d017882589994fef32d3b5e
Tyler Cook <twc35917@uga.edu>
2024-09-11 21:58:52 -0400

    "added micro"

--- BlinkLibrary/Class/blinkClass.c
+++ BlinkLibrary/Class/blinkClass.c
@@ -1,6 +1,7 @@
 #include "E4235.h"
 #include <stdio.h>
 //gcc blinkClass.c -o blinkClass -lE4235
+//gcc blinkClass.c -o blinkClass E4235_Delaynano.s E4235_Select.s E4235_Write.s
 extern int E4235_Write ( int GPIO, int state );
 extern int E4235_Select ( int GPIO, int state );
 extern void E4235_DelayMicro ( int count )
