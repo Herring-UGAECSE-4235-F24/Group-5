0c1a44bf32e39ad6c1e41fb597cb9d405837e92c
Tyler-W-Cook <twc35917@uga.edu>
2024-09-11 21:51:17 -0400

    crashing out

--- BlinkLibrary/Class/blinkClass.c
+++ BlinkLibrary/Class/blinkClass.c
@@ -5,13 +5,13 @@
 
 int main() {
 E4235_Select(17, 1); // set GPIO 17 to output
-int pins[1] =	{17};
+int pins[1] = {17};
 while (1) {
-	E4235_multiwrite(17,1, 1);
+	E4235_multiwrite(pins,1, 1);
 	
 	E4235_Delaynano(10000000);
 	
-	E4235_multiwrite(17,1, 0);
+	E4235_multiwrite(pins, 1, 0);
 	
 	E4235_Delaynano(10000000);
 
