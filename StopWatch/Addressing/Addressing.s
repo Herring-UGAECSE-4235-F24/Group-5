98a87ac2184f1d118a89ab0b8f2313f056bcd96b
Tyler-W-Cook <twc35917@uga.edu>
2024-09-15 14:30:17 -0400

    completed addressing.s

--- StopWatch/Addressing/Addressing.s
+++ StopWatch/Addressing/Addressing.s
@@ -1,5 +1,5 @@
 @ This is an individual assignment
-@Deliverable 1:  Describe the function of the program.
+@ Deliverable 1:  Describe the function of the program.
 @ Now, let's investigate what you can and can't do on the instructions.
 @ Deliverable 2: Can you replace each [Rx] with the value?  For example replace [R6] with [0x90]?
 @ Deliverable 3: Try it for each instance for LDR and STR.  Results?
@@ -8,13 +8,18 @@
 	.text
 	.global main
 	
-main:	LDR R6,=0x90		@R6 = 0x90
+main:
+	LDR R6,[SP]		@R6 = 0x90
   	LDR R1,[R6]	 	@R1 = [0x90]
-  	LDR R6,=0x94	 	@R6 = 0x94
-  	LDR R2,[R6]	  	@R1 = [0x94]
-  	ADD R2,R2,R1	  	@R2 = R2 + R1
-  	LDR R6,=0x300	  	@R6 = 0x300
- 	STR R2,[R6]	  	@[0x300] = R2
- 	
+	SUBS SP, SP, #4
+  	LDR R6,[SP]	 	@R6 = 0x94
+	LDR R2,[R6]	  	@R2 = [0x94]
+  	ADD R2,R2,R1
+	SUBS SP, SP, #4	  	@R2 = R2 + R1
+  	LDR R6,[SP]	  	@R6 = 0x300
+ 	STR R2,[SP]	  	@[0x300] = R2
+ 	ADDS SP, SP, #4
+	ADDS SP, SP, #4
 		mov   	r7, #1
 		svc  	0
+
