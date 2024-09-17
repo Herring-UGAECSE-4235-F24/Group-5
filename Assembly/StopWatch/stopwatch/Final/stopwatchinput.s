2b31b891729cf640188e28d2e79451eb33b98493
Tyler Cook <twc35917@uga.edu>
2024-09-16 22:20:13 -0400

    added keyblock and different states

--- Assembly/StopWatch/stopwatch/Final/stopwatchinput.s
+++ Assembly/StopWatch/stopwatch/Final/stopwatchinput.s
@@ -11,48 +11,46 @@
 
 main:
 	b _c
-	
-_reset:
-	ldr r4 , =hundredths 
-	ldr r5 , =seconds
-	ldr r6 , =minutes
-	mov r7, #0 @hundredth of a second count when hits 12,000 reset to 0 (aka hit 2 mins)
-	mov r8, #0 @seconds
-	mov r9, #0 @minutes
-	str r7, [r4] @using zero reg to reset
-	str r8, [r5]
-	str r9, [r6]
 
-
-	
-	
+resetChar:
+	ldr r1, =char
+	mov r2, #0
+	str r2, [r1]
+	bx lr
+checkScan:
+	ldr r1, =char                   @loads address of returned char back into r1
+	ldrb r1, [r1]
+    cmp r1, #'r' 
+	beq r
+	cmp r1, #'c'
+	beq c 
+	cmp r1, #'l'
+	beq l
+	cmp r1, #'s'
+	beq s
+	bx lr
+display_time:
+	LDR R0, =string         @ seed printf
+    LDR R1, =minutes 
+    LDR R2, =seconds		@ loads mins into R1 for print
+    LDR R3, =hundredths
+    LDR R1, [R1]            @ seed printf
+    LDR R2, [R2]            @ seed printf
+    LDR R3, [R3]            @ seed printf
+    BL printf
 _reload:
-	ldr r10, =9077500 @whatever time is a hunredth of a sec based on delay loop 
+	ldr r3, =6000000 @whatever time is a hunredth of a sec based on delay loop 
 	add r7, r7, #1 @increments every hundreth of a second
 	str r7, [r4] @strs back to hundreths .data
 	cmp r7, #100
 	BEQ _incrementSec
 	
 _delayloop:
-	subs r10, r10, #1
+	bl checkScan
+	subs r3, r3, #1
 	bne  _delayloop
+	b _reload
 	
-_printloop:
-	@Prints mins
-    	LDR R0, =string         @ seed printf
-    	LDR R1, =minutes 
-        LDR R2, =seconds		@ loads mins into R1 for print
-        LDR R3, =hundredths
-    	LDR R1, [R1]            @ seed printf
-        LDR R2, [R2]            @ seed printf
-        LDR R3, [R3]            @ seed printf
-    	BL printf
-
-		cmp R9, #2
-		beq _reset @if 2:00:00 is printed reset to 0:0:0
-
-   	B _reload @else countnue incrementing hundreths
-
 _incrementSec:
 	mov R7, #0
 	str R7, [R4] 		@hundreths is reset to 0 in mem
@@ -60,23 +58,50 @@ _incrementSec:
 	str R8, [R5]		@seconds is incremented in mem
 	cmp R8, #60			@if 60 secs increment min
 	beq _incrementMin
-	bne _printloop
+	
 _incrementMin:
 	mov R8,	#0 			@resets seconds to zero in mem
 	str R8, [R5]
 	add R9, R9, #1
 	str R9, [R6] 		@increments min in mem		
-	b _printloop
 	
+	ldr r1, =Lap
+	ldr r1, [r1]
+	cmp r1, #0
+	beq display_time
+	b _reload
 
 
 _s:
 	@if stop using blocking of scanf 
+	bl resetChar
+	bl block
+	ldr r0, =format
+    ldr r1, =char
+    Bl scanf				@keyblock on so it waits for input to start
+	ldr r1, =char                   @loads address of returned char back into r1
+    ldrb r1, [r1]
+    cmp r1, #'r' 
+	beq r
+	cmp r1, #'c'
+	beq c
+	b s
 	@if scan f returns r continue from previous state
 	@if L do nothing
 	@if C reset
 _l:
 	@stop printing but count is running in the background still
+	bl resetChar
+	bl deblock
+	mov r1, #1
+	ldr r2, =Lap
+	str r1, [r2]  @used to check if lap or just run to decide if we need to display or not
+
+	ldr r0, =format
+    ldr r1, =char
+    Bl scanf
+	b _reload
+
 	@if scanf retuns L nothing
 	@if scanf returns r continue count from backgrounf running time
 	@if scanf returns c clear count and 
@@ -99,57 +124,55 @@ _c:
     LDR R3, [R3]            @ seed printf
     BL printf
 							@turn keyblock on to wait for scanf
-	BL _KeyBlockOn
+	bl block
+wait:	
 	ldr r0, =format
     ldr r1, =char
     Bl scanf				@keyblock on so it waits for input to start
-	ldr 
-							@cmps to determine which state is next
-	bl _KeyBlockOff			@keyblock is now off so we can read scanf while waiting
+	ldr r1, =char                   @loads address of returned char back into r1
+    ldrb r1, [r1]
+    cmp r1, #'r' 
+	beq r
+	b wait
 
 _r:
-	b main
+	bl resetChar
+	bl deblock
+	mov r1, #0
+	ldr r2, =Lap
+	str r1, [r2]  @used to check if lap or just run to display
+	ldr r0, =format
+    ldr r1, =char
+    Bl scanf
+	b _reload
 
 
-
-_KeyBlockOff:		@from class lib
-    mov r0, #0 	    @ file descriptor for stdin
-	mov r1, #3	    @ get F_GETFL
-	bl fcntl
-	mvn r2, #2048	@ set inverse O_NONBLOCK
-	and r1, r1, r2	@ combine flags
-	mov r2, r1
-	mov r0, #0	    @ file descriptor for stdin
-	mov r1, #4	    @ set F_SETFL
-    bl fcntl
-	bx lr
-_KeyBlockOn: 		@from class lib
-    mov r0, #0 	    @ file descriptor for stdin
-	mov r1, #3	    @ get F_GETFL
-	bl fcntl
-	mov r2, #2048	@ set O_NONBLOCK
-	orr r1, r1, r2	@ combine flags
-	mov r2, r1
-	mov r0, #0	    @ file descriptor for stdin
-	mov r1, #4	    @ set F_SETFL
-    bl fcntl
-	bx lr
-c:
 _exit:
 	mov     R0, #0          @use 0 return code
 	mov     R7, #1          @service command code 1 
 	svc     0               @call linux to terminate
 
+deblock:
+    mov r0, #1    @sets deblock to 1
+    bl E4235_KYBdeblock
+	bx lr
+block:
+	mov r0, #0
+	bl E4235_KYBdeblock	
+	bx lr
+
 .data
 char:
-		.asciz ""
+		.byte  0
 string:
        	.asciz "%02d:%02d:%02d\n"
 format:
-		.asciz "%c"			 	@for reading char	
+		.asciz " %c"			 	@for reading char	
 hundredths:
 	    .word   0               @ hundreths count storage for printing
 seconds:
 		.word 	0 				@ seconds count storage for print
 minutes:
 		.word	0				@ minutes count for print
+Lap:
+		.word   0
