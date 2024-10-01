23e9877b2bc9f8cc8a47c054f0852bb2e843879b
Tyler Cook <twc35917@uga.edu>
2024-09-30 22:32:10 -0400

    added some sudo code and took time to plan the project out

--- C/StateMachine/StateMachine.c
+++ C/StateMachine/StateMachine.c
@@ -12,9 +12,25 @@ char keys[ROWS][COLS] = {
   {'*', '0', '#', 'D'}
 };
 
+//to get correct ascii number we can just convert char to int to retrieve ascii value
+
 // Define the row and column pin connections
-byte rowPins[ROWS] = {5, 8, 7, 6}; // Connect to the row pins of the keypad
-byte colPins[COLS] = {9, 10, 0, 2};  // Connect to the column pins of the keypad
+byte rowPins[ROWS] = {20, 21, 22, 23}; // Connect to the row pins of the keypad
+byte colPins[COLS] = {24, 25, 26, 27};  // Connect to the column pins of the keypad
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-char key = keypad.getKey(); // Get the key pressed
+
+
+int main(){
+  microsleep(1000);// will give 1khz clock
+  //output gpio 9 high
+  char key = keypad.getKey(); // Get the key pressed we can use the GPIO 9 to replace the msb output for a clock to our analyzer if needed.
+  int ascii = key; //this will convert the char into ascii
+  int GPIO = 17;
+  while(ascii>0){
+    int output = ascii % 2;
+    write(GPIO, output) //outputs high or low depending on first bit
+    ascii = ascii>>1;
+    GPIO --;
+  }
+}
