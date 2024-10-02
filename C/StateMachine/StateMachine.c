0a8cb53b8431b0e38a6455536b2d3a719bdc84fb
Tyler Cook <twc35917@uga.edu>
2024-10-01 20:44:14 -0400

    added core functionality implemented via bit shifting

--- C/StateMachine/StateMachine.c
+++ C/StateMachine/StateMachine.c
@@ -1,15 +1,21 @@
 //Commented out because this is only used for Arduino IDE libraries which is what I was using. I created methods below that were included in the header file.
 //#include <Keypad.h>
-
+//#include "E4235.h"
+#include <stdio.h>
+//gcc blinkClass.c -o blinkClass -lE4235
+//gcc blinkClass.c -o blinkClass E4235_Delaynano.s E4235_Select.s E4235_Write.s
+extern int E4235_Write(int GPIO, int state);
+extern int E4235_Select(int GPIO, int state);
+extern int E4235_Read(int GPIO);
+extern void E4235_DelayMicro(int count);
 // Define the row and column pins
-const byte ROWS = 4; // Four rows
-const byte COLS = 4; // Four columns
-
-//to get correct ascii number we can just convert char to int to retrieve ascii value
+const int ROWS = 4; // Four rows
+const int COLS = 4; // Four columns
 
 // Define the row and column pin connections
-byte rowPins[ROWS] = {20, 21, 22, 23}; // Connect to the row pins of the keypad
-byte colPins[COLS] = {24, 25, 26, 27};  // Connect to the column pins of the keypad
+int rowPins[ROWS] = {20, 21, 22, 23}; // Connect to the row pins of the keypad
+int colPins[COLS] = {24, 25, 26, 27};  // Connect to the column pins of the keypad
+
 
 
 // Define the keymap
@@ -23,35 +29,92 @@ char getKey(int x, int y) {
   return keys[y][x];
 }
 
+
 //Read the Keypad
 char keypadRead() {
   for (int i = 0; i < 4; i++) {
-	  digitalWrite(rowPins[i], 1); //high output 1
+	  E4235_Write(rowPins[i], 1); //high output 1
 	  for (int j = 0; j < 4; j++) {
-	    if (digitalRead(colPins[j] == 1) { //high output 1
-		    delay(.5); //debounce delay of half a second
-		    if(digitalRead(colPins[j] == 1) { //high output 1
-			    digitalWrite(rowPins[i], 0); //low output 0
-			    return getKey(x,y);
+	    if (E4235_Read(colPins[j]) == 1) { //high output 1
+		    E4235_DelayMicro(500000); //debounce delay of half a second
+		    if(E4235_Read(colPins[j]) == 1) { //high output 1
+			    E4235_Write(rowPins[i], 0); //low output 0
+			    return getKey(i,j);
 		    }
 	    }
     }
-  digitalWrite(rowPins[i], 0); //low output 0
+  E4235_Write(rowPins[i], 0); //low output 0
   }
 return 0;
 }
 
-int main(){
-  microsleep(1000);// will give 1khz clock
-  //output gpio 9 high
-  //commented out below and created new getkey method above
-  //char key = keypad.getKey(); // Get the key pressed we can use the GPIO 9 to replace the msb output for a clock to our analyzer if needed.
-  int ascii = key; //this will convert the char into ascii
-  int GPIO = 17;
+void asciiMode(char key){
+ int GPIO = 10;
+ int ascii = key;
   while(ascii>0){
+    if(ascii>0){
     int output = ascii % 2;
-    write(GPIO, output) //outputs high or low depending on first bit
+    E4235_Write(GPIO, output); //outputs high or low depending on first bit
     ascii = ascii>>1;
-    GPIO --;
+    } else{
+      E4235_Write(GPIO,0);
+    }
+    GPIO ++;
+  }
+}
+
+void binaryMode(char key){
+int GPIO = 10;
+int binary = 40; //default
+  if (isDigit(key)){
+  int binary = key -'0'; // char conversion to int
+  }else if(key == 'A'){
+    int binary = 10;
+  } else if(key == 'B'){
+    int binary = 11;
+  } else if(key == 'C'){
+    int binary = 12;
+  } else if(key =='D'){
+    int binary = 13;
+  }
+    while(GPIO>18){
+      if(binary>0){
+        int output = binary % 2; //gives lsb to output
+        E4235_Write(GPIO, output); //outputs high or low depending on first bit
+        binary = binary>>1; //right shift to
+      } else {
+      E4235_Write(GPIO,0);
+      }
+      GPIO ++;
+    }
+}
+
+//to get correct ascii number we can just convert char to int to retrieve ascii value
+int main(){
+ // Define the row and column pins as ouputs and inputs
+  for(int i = 0; i < ROWS; i++) {
+  E4235_Select(rowPins[i], 1); //setting rows to be outputs
+  E4235_Select(colPins[i], 0); //setting cols as inputs
+  }
+
+  int swapMode = 0;
+  while(1){
+    E4235_DelayMicro(500);// will give 1khz clock
+    //output gpio 9 high
+    E4235_Write(9,1);
+    //Get the key pressed we can use the GPIO 9 to replace the msb output for a clock to our analyzer if needed.
+    char key =  keypadRead(); //this will convert the char into ascii
+    if(key == '#'){
+      swapMode++;
+    } else if (key == '*'){
+      swapMode = 0;
+      key = '@';
+    } else if(swapMode % 2 == 0){ //aka default 
+      binaryMode(key);
+    } else {
+      asciiMode(key);
+    }
+  E4235_DelayMicro(500);
+  E4235_Write(9,0);
   }
 }
