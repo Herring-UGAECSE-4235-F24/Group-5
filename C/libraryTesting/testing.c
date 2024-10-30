e61408cd54c3eb2493f3de831b6122d83b89aa8f
Tyler Cook <twc35917@uga.edu>
2024-10-30 17:04:17 -0400

    please work now

--- C/libraryTesting/testing.c
+++ C/libraryTesting/testing.c
@@ -7,14 +7,14 @@
 #define PIN RPI_GPIO_P1_11
 //gcc testing.c -o testing E4235_Read.s -Wall -lbcm2835
  // Set the pin to be an output to test read
-extern int E4235_Read (int);
-
+extern int E4235_Read (int GPIO);
+extern int E4235_Read (int GPIO, int state);
 int main(){
 	if(!bcm2835_init()){
 		return 1;
 	}
 	
-
+    E4235_Select(16,0);          // 0 for input    
 	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
 	int read = 0;
 	while(1){
