0707ac0d65d5b18fbdcc5fef80ca558d0b1b725d
Tyler-W-Cook <twc35917@uga.edu>
2024-10-30 16:47:15 -0400

    read L

--- C/libraryTesting/testing.c
+++ C/libraryTesting/testing.c
@@ -2,43 +2,32 @@
 #include <stdlib.h>
 #include "bcm2835.h"
 #include <unistd.h>
+#include "E4235.h"
 
 #define PIN RPI_GPIO_P1_11
-//gcc testing.c -o testing E4235_Read.s
+//gcc testing.c -o testing E4235_Read.s -Wall -lbcm2835
  // Set the pin to be an output to test read
-extern int E4235_Read ( int GPIO);
+extern int E4235_Read (int);
+
 int main(){
 	if(!bcm2835_init()){
 		return 1;
 	}
 	
-	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
-	int freq;
-    	printf("Enter Freq in Hz: ");
-    	if (scanf("%d", &freq) != 1) {
-        	printf("Error: Invalid input\n");
-        	exit(1);
-    	}
-	 
-	printf("your freq is %d", freq);
 
+	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
+	int read = 0;
 	while(1){
 	bcm2835_gpio_write(PIN , HIGH); //1 = high
-    if(E4235_Read(16)){
-        printf("HIGH");
-    } else {
-        printf("LOW");
-    }
-
-	bcm2835_delayMicroseconds(1);		//delay in mils
+	read = E4235_Read(16);
+	printf("%d", read);
+	bcm2835_delayMicroseconds(5000000);		//delay in mils
 	
 	bcm2835_gpio_write(PIN , LOW); //0 = low
-	 if(E4235_Read(16)){
-        printf("HIGH");
-    } else {
-        printf("LOW");
-    }
-	bcm2835_delayMicroseconds(1);
+	read = E4235_Read(16);
+	printf("%d", read);
+	bcm2835_delayMicroseconds(5000000);
+	printf("next loop\n");
 	}
 	
 }
