5df76b585189094f8d0be0fb6abcb4a2a38cfbf8
Tyler Cook <twc35917@uga.edu>
2024-10-31 10:25:30 -0400

    rewrote test

--- C/libraryTesting/testing.c
+++ C/libraryTesting/testing.c
@@ -6,33 +6,32 @@
 
 #define PIN1 RPI_GPIO_P1_11
 #define PIN2 RPI_GPIO_P1_15
-//gcc testing.c -o testing E4235_Read.s E4235_Select.s -Wall -lbcm2835
+//gcc testing.c -o testing E4235_Read.s E4235_Select.s E4235_Write.s E4235_DelayMicro.s -Wall 
  // Set the pin to be an output to test read
 extern int E4235_Read (int GPIO);
 extern int E4235_Select (int GPIO, int state);
+extern int E4235_Write (int GPIO, int state);
+extern void E4235_DelayMicro ( int count );
 
 int main(){
-	if(!bcm2835_init()){
-		return 1;
-	}
+
 	
-	//E4235_Select(22,0);          // 0 for input    
-	bcm2835_gpio_fsel(PIN1, BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_fsel(PIN2, BCM2835_GPIO_FSEL_INPT);
+	E4235_Select(22,0);          // 0 for input    
+	E4235_Select(17,1);  		  // 1 for output
+
 	int read = 0;
 	while(1){
-	bcm2835_gpio_write(PIN1 , HIGH); //1 = high
 	
-	bcm2835_delayMicroseconds(5000000);		//delay in mils
-	//read = E4235_Read(22);
-	//read = bcm2835_gpio_lev(PIN2);
+	E4235_Write(17, 1);
+	read = E4235_Read(22);
 	printf("%d\n", read);
 	
-	bcm2835_gpio_write(PIN1 , LOW); //0 = low
-	//read = E4235_Read(22);
-	read = bcm2835_gpio_lev(PIN2);
+	E4235_DelayMicro(5000000);
+	E4235_Write(17, 0);
+	
+	read = E4235_Read(22);
 	printf("%d\n", read);
-	bcm2835_delayMicroseconds(5000000);
+	E4235_DelayMicro(5000000);
 	
 	}
 	
