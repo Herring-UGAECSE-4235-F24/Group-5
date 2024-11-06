5c6eab1e428d1101f0226363597097c16255e993
Tyler Cook <twc35917@uga.edu>
2024-11-06 17:48:32 -0500

    Pushing laptop code for fpEx1 and SinTaylor series that I modified to meet the deliverable requirements

---
+++ Assembly/FloatingPoint/SeriesFunctions/exponential.s
@@ -0,0 +1,20 @@
+
+    ldr r0, =format
+    ldr r1, =char
+    Bl scanf				@keyblock on so it waits for input to start
+
+
+char:
+		.byte  0
+string:
+       	        .asciz "%02d:%02d:%02d\n"
+format:
+		.asciz " %c"			 	@for reading char	
+hundredths:
+	        .word   0               @ hundreths count storage for printing
+seconds:
+		.word 	0 				@ seconds count storage for print
+minutes:
+		.word	0				@ minutes count for print
+Lap:
+		.word   0
---
+++ Assembly/FloatingPoint/fpEx1/fpEx1.s
@@ -0,0 +1,31 @@
+@ Group assignment
+@ This program calculates the area of a circle
+@ use gdb info all-registers  or i all-r
+@ Change the code to print the results to the monitor in correct format.
+@ We will check in class.  Be ready to change the radius value and re-run
+
+
+	.text
+	.global _start
+_start:
+	vmov.f32	s0, #0.125
+	vmul.f32 	s0, s0, s0		@r^2
+	ldr	r2,=piNumber
+	vldr		s1, [r2]
+	vmul.f32	s0, s0, s1		@area
+	
+	vcvt.f64.f32 d0, s0		@ fp to double conversion
+	ldr r0, =string
+	vmov r1, r2, d0
+	bl printf
+	
+exit: 
+	mov R7, #1          @service command code 1 
+	svc     0               @call linux to terminate
+
+.data
+string:
+    .asciz "The area of your circle is:  %f"
+
+piNumber:
+	.float 3.141593
