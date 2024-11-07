1b6ffc9a92a7ac961fe5d0e47e6b8d9649f6bc9c
Tyler-W-Cook <twc35917@uga.edu>
2024-11-06 19:48:05 -0500

    bus error for scanf in sinTay

--- Assembly/FloatingPoint/SinTaylorSeries/SinTaylorSeries.s
+++ Assembly/FloatingPoint/SinTaylorSeries/SinTaylorSeries.s
@@ -8,68 +8,75 @@
 @We will check code on Github.  
 
 @ compiled using =>gcc SinTaylorSeries.s -o SinTaylorSeries -mfpu=vfpv3
-	.global main
-    .func main
-
-main:
-	ldr r0, =float			@format for scan float
-	ldr r1, =x			@ stores input in x
-	bl scanf
-	
-	ldr r1, =x			@storing input into a single register
-	vldr s0, [r1]
-
-
-    vmrs r1, fpscr @ get FPSCR contents in r1
-    bic r2, r1, #(0b1111<<23)
-    orr r2, r2, #(0b1100<<23)
-    vmsr fpscr, r2 @ store settings in FPSCR
-    vmul.f32 s1,s0,s0 @ s1 = x^2
-    ldr r0,=ctab @ get address of coefficients
-    vldmia r0!,{s16-s20} @ load all coefficients into Bank 2
-    vmul.f32 s8,s0,s1 @ s8 = x^3
-    vmul.f32 s9,s8,s1 @ s9 = x^5
-    vmul.f32 s10,s9,s1 @ s10 = x^7
-    vmul.f32 s11,s10,s1 @ s11 = x^9
-    vmul.f32 s12,s11,s1 @ s12 = x^11
-    @@ Set VFP for vector mode
-    bic r2, r2, #(0b11111<<16) @ set rounding, stride to 1,
-    orr r2, r2, #(0b00100<<16) @ and vector length to 5
-    vmsr fpscr, r2 @ store settings in FPSCR
-    vmul.f32 s24,s8,s16 @ VECTOR operation x^(2n+1) * coeff[n]
-    vmsr fpscr, r1 @ restore original FPSCR
-    @@ Add terms in Bank 3 to the result in s0
-    vadd.f32 s24,s24,s25
-    vadd.f32 s26,s26,s27
-    vadd.f32 s0,s0,s24
-    vadd.f32 s26,s26,s28
-    vadd.f32 s0,s0,s26
-    mov pc,lr		@can also use bx lr
-
-
-
-    vcvt.f64.f32 d0, s0		@ fp to double conversion
-    ldr r0, =string
-    ldr r1, =x
-    ldr r1, [r1]
-    vmov r2, r3, d0
-    bl printf
-
-exit:
-    mov R7, #1
-    svc #0
-
-.data
-    x:
-		.word 0						@storing input
-	string:
-		.asciz "Sin(%f) = %f\n"		@output statement
-	float:
-		.asciz " %f"					@formatting scanf for floats
-
-    ctab: 
+	.data
+   
+	prompt:
+                .asciz "Sin(x):   x = "
+        x:
+                .float 0                                                @storing input
+        string:
+                .asciz "Sin(x) = %f\n"          @output statement
+        float:
+                .asciz "%f"                                     @formatting scanf for floats
+        ctab: 
         .word 0xBE2AAAAB @ -1.666667e-01
         .word 0x3C088889 @ 8.333334e-03
         .word 0xB9500D01 @ -1.984127e-04
         .word 0x3638EF1D @ 2.755732e-06
         .word 0xB2D7322B @ -2.505211e-08
+
+	.text
+	.global main
+	.func main
+main:
+	ldr r0, =prompt
+	bl printf
+
+
+	ldr r0, =float			@format for scan float
+	ldr r1, =x
+	bl scanf
+
+	ldr r0, =prompt
+	bl printf
+	
+	ldr r0, =x			@storing x address then loading x into s0
+	vldr s0, [r0]
+
+
+
+        vmrs r1, fpscr @ get FPSCR contents in r1
+        bic r2, r1, #(0b1111<<23)
+        orr r2, r2, #(0b1100<<23)
+        vmsr fpscr, r2 @ store settings in FPSCR
+        vmul.f32 s1,s0,s0 @ s1 = x^2
+        ldr r0,=ctab @ get address of coefficients
+        vldmia r0!,{s16-s20} @ load all coefficients into Bank 2
+        vmul.f32 s8,s0,s1 @ s8 = x^3
+        vmul.f32 s9,s8,s1 @ s9 = x^5
+        vmul.f32 s10,s9,s1 @ s10 = x^7
+        vmul.f32 s11,s10,s1 @ s11 = x^9
+        vmul.f32 s12,s11,s1 @ s12 = x^11
+	    @@ Set VFP for vector mode
+        bic r2, r2, #(0b11111<<16) @ set rounding, stride to 1,
+        orr r2, r2, #(0b00100<<16) @ and vector length to 5
+        vmsr fpscr, r2 @ store settings in FPSCR
+        vmul.f32 s24,s8,s16 @ VECTOR operation x^(2n+1) * coeff[n]
+        vmsr fpscr, r1 @ restore original FPSCR
+   	 @@ Add terms in Bank 3 to the result in s0
+        vadd.f32 s24,s24,s25
+        vadd.f32 s26,s26,s27
+        vadd.f32 s0,s0,s24
+        vadd.f32 s26,s26,s28
+        vadd.f32 s0,s0,s26
+
+
+    	vcvt.f64.f32 d0, s0		@ fp to double conversion
+
+    	ldr r0, =string
+    	vmov r1, r2, d0
+   	bl printf
+
+exit:
+	mov r7, #1
+	svc #0
